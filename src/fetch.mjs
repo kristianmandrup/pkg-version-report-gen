1c34dd6aa6a13d903bdbbdb9f0d6687f1e9faada
Kristian Mandrup <kristian.mandrup@hogarthww.com>
2021-11-05 13:57:09 +0000

    fix date diff and version sort

--- src/fetch.mjs
+++ src/fetch.mjs
@@ -1,12 +1,16 @@
 import { pkgInfo } from './package-info.mjs'
 import {readPackage} from 'read-pkg';
+import * as fs from 'fs'
 
 const getInfo = async (name, version, opts = {}) => {
   return await pkgInfo(name, version, opts)
 }
 
+
+
 export const getPkgDependencies = async (pkgFilePath) => {
-  const data = await readPackage(pkgFilePath)
+  var content=fs.readFileSync(pkgFilePath, 'utf8');    
+  const data = JSON.parse(content);
   return data.dependencies;
 }
 
--- src/package-info.mjs
+++ src/package-info.mjs
@@ -1,6 +1,9 @@
 'use strict';
 import got from 'got'
 import registryUrl from 'registry-url';
+import pkg from 'semver';
+import diffDates from "diff-dates";
+const { compare, valid } = pkg;
 
 const maintainerNames = (maintainers) => maintainers && maintainers.reduce((acc, maintainer) => {
 		acc.push(maintainer.name);
@@ -8,7 +11,7 @@ const maintainerNames = (maintainers) => maintainers && maintainers.reduce((acc,
 	}, []).join(', ')
 
 const excludeKeys = ['modified', 'created']
-const versionsFor = (time) => Object.keys(time).filter(key => !excludeKeys.includes(key))
+const versionsFor = (time) => Object.keys(time).filter(key => !excludeKeys.includes(key)).filter(v => v);
 
 const dateFor = (date) => {
 	if (!date) return date
@@ -16,13 +19,16 @@ const dateFor = (date) => {
 	return `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
 }
 
-const diffDates = (d1, d2) => {
-	var date1 = new Date(d1);
-	var date2 = new Date(d2);
-	return date2.getDate() - date1.getDate();	
-}
+const sortVersions = (versions, loose = false) => Array.from(versions).filter(x => valid(x)).sort((a, b) => compare(a, b, loose))
 
 export const pkgInfo = (name, version, opts = {}) =>{
+	const dayDiff = (date1, date2) => {
+		const d1 = new Date(date1)
+		const d2 = new Date(date2)
+		const days = diffDates(d2, d1, "days");
+		return days || 0;
+	}
+	
 	if (typeof name !== 'string') {
 		return Promise.reject(new Error('package name required'));
 	}
@@ -31,19 +37,22 @@ export const pkgInfo = (name, version, opts = {}) =>{
 			const dataParsed = JSON.parse(data.body);
 			const name = dataParsed.name || '';
 			const time = dataParsed.time || {};
-			const latestVersion = dataParsed[ 'dist-tags' ].latest || '';
-			const latestVersionDate = dateFor(time['' + latestVersion])  || '';
+			// const latestVersion = dataParsed[ 'dist-tags' ].latest || '';
+			const allVersions = versionsFor(time)
+			const sortedVersions = sortVersions(allVersions || [])
+			const versions = sortedVersions.slice(-5)
+			const latestVersion = sortedVersions[sortedVersions.length - 1];
+			const latestVersionDt = time['' + latestVersion]
+			const latestVersionDate = dateFor(latestVersionDt)  || '';
 
-			const hasCaret = version.match(/^/);
+			const hasCaret = version.match(/\^/);
 			const firstMatchingVersion = hasCaret ? version.slice(1) : version;
-
-			const versionDate = dateFor(time['' + firstMatchingVersion]) || ''			
+			const versionDt = time['' + firstMatchingVersion]
+			const versionDate = dateFor(versionDt) || ''			
 			const description = dataParsed.description || '';
 			const license = dataParsed.license || '';
 			const homepage = dataParsed.homepage || ''
-			const versions = versionsFor(time).slice(-5)
-
-			const daysBehindLatestVersion = diffDates(latestVersionDate, versionDate)
+			const daysBehindLatestVersion = dayDiff(versionDt, latestVersionDt) || 0;
 
 			const authorName  = dataParsed.author?.name || 
 			maintainerNames(dataParsed.maintainers) || '';
